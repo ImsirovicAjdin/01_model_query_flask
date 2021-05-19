c256738a41d6551446236363f07f6fe9a34db6c8
Ajdin Imsirovic <ajdika@live.com>
2021-05-19 07:01:15 +0200

    08. Connect to the db from our Flask app (by setting a configuration variable)

--- model_query.py
+++ model_query.py
@@ -1,12 +1,13 @@
 # start by adding a new db in bash: createdb model_query
 # mkdir model_query && cd model_query && touch model_query.py && code .
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask # 01. Import flask
+from flask_sqlalchemy import SQLAlchemy # 05. Import the SQLAlchemy module
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
+app = Flask(__name__) # 02. Instantiate the app
+db = SQLAlchemy(app) # 06. Instantiate the db and pass in this flask app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///model_query' # 08. Connect to the db from our Flask app (by setting a configuration variable)
 
-@app.route('/')
-def index():
-    return "Hello model query!"
+@app.route('/') # 03. With at app decorator, listen to root route
+def index(): # 04. Define the index method...
+    return "Hello model query!" # 04. ... and return a string from it
